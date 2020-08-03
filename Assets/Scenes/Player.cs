a47fa02b8c7453ac06f266a58aa06155516d933d
Abhay Raizada <toabhayraizada@gmail.com>
2020-08-03 18:00:19 +0530

    first commit

---
+++ Assets/Scenes/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+     float speed = 1.0f;
+
+     void Update()
+     {
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        if((move.x !=0 || move.y != 0) && speed < 25.0f) {
+            speed += 1.0f;
+        }
+        else{
+            speed = 1.0f;
+        }
+        transform.position += move * speed * Time.deltaTime;
+     }
+}
